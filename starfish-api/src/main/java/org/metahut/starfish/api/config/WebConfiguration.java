2928f2dc24d2e31017cc068eb9e6f6aa0b1a1e92
zhuangchong <zhuangchong6@163.com>
2022-03-03 18:28:18 +0800

    update common config in the api module

---
+++ starfish-api/src/main/java/org/metahut/starfish/api/config/WebConfiguration.java
@@ -0,0 +1,40 @@
+package org.metahut.starfish.api.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+
+import static org.metahut.starfish.api.utils.Constants.LOCALE_COOKIE;
+import static org.metahut.starfish.api.utils.Constants.LOCALE_LANGUAGE;
+
+@Configuration
+public class WebConfiguration implements WebMvcConfigurer {
+
+    @Bean
+    public LocaleResolver localeResolver() {
+        CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+        localeResolver.setCookieName(LOCALE_COOKIE);
+        localeResolver.setDefaultLocale(Locale.ENGLISH);
+        localeResolver.setCookieMaxAge(3600);
+        return localeResolver;
+    }
+
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
+        localeChangeInterceptor.setParamName(LOCALE_LANGUAGE);
+        return localeChangeInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
+    }
+
+}
---
+++ starfish-api/src/main/java/org/metahut/starfish/api/utils/Constants.java
@@ -0,0 +1,12 @@
+package org.metahut.starfish.api.utils;
+
+public class Constants {
+
+    private Constants() {
+
+    }
+
+    public static final String LOCALE_LANGUAGE = "lang";
+    public static final String LOCALE_COOKIE = "localeCookie";
+
+}
