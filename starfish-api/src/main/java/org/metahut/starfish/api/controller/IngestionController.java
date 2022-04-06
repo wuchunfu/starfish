3679534d4b51e408e71eb9d0cb5aaa2f915ae1b1
calvin <jianghuachinacom@163.com>
2022-04-06 17:47:25 +0800

    improve the style of coding

--- starfish-api/src/main/java/org/metahut/starfish/api/controller/IngestionController.java
+++ starfish-api/src/main/java/org/metahut/starfish/api/controller/IngestionController.java
@@ -1,6 +1,7 @@
 package org.metahut.starfish.api.controller;
 
 import org.metahut.starfish.api.dto.ResultEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
